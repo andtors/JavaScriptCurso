3398ee8ddfed44dd1132c4a46b36ac8cee586be0
André Torres <andre.tors448@gmail.com>
2024-01-15 11:39:17 -0300

    SC-14-CSS Propriedade

--- SC-8-Array/concat.js
+++ SC-8-Array/concat.js
@@ -5,3 +5,15 @@ console.log(todos, filhas, filhos)
 
 
 console.log(['a', 'b'].concat([1,2], [3, 4], 5, [[6, 7]]))
+
+const array1 = [1, 2 ,3]
+
+function concatenarTudo(array){
+    let total = ''
+    for(let i=0; i<=array.length;i++){
+       total += [i].toString()
+    }
+    console.log(total)
+}
+
+concatenarTudo(array1)
