27a2d31918dde048db3c9322ffc8bfc3e0e3e079
André Torres <andre.tors448@gmail.com>
2024-01-26 12:01:23 -0300

    SC-18-Webpack iniciado

---
+++ SC-18-Webpack/src/principal.js
@@ -0,0 +1,10 @@
+//const Pessoa = require('./pessoa')
+//const Pessoa = require('./pessoa')
+//const moduloA = require('./modulos/moduloA')
+
+import Pessoa from './pessoa'
+import './assets'
+
+
+const atendente = new Pessoa
+console.log(atendente.cumprimentar())
---
+++ SC-18-Webpack/webpack.config.js
@@ -0,0 +1,49 @@
+const modoDev = process.env.NODE_ENV !== 'production'
+const webpack = require('webpack')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+ 
+module.exports = {
+    mode: modoDev ? 'development' : 'production',
+    entry: './src/principal.js',
+    output: {
+        filename: 'principal.js',
+        path: __dirname + '/public'
+    },
+    optimization: {
+        minimizer: [
+            (compiler) => {
+                const TerserPlugin = require('terser-webpack-plugin');
+                new TerserPlugin({
+                  terserOptions: {
+                    compress: {},
+                  }
+                }).apply(compiler);
+              },
+            new CssMinimizerPlugin({})
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "estilo.css"
+        }),
+        new TerserPlugin({
+            parallel: true,
+            terserOptions: {
+                ecma: 6,
+            },
+        }),
+    ],
+    module: {
+        rules: [{
+            test: /\.s?[ac]ss$/,
+            use: [
+                MiniCssExtractPlugin.loader,
+                //'style-loader', //adiciona a css a dom injetando a tag <style> 
+                'css-loader', // interperta o @import
+                'sass-loader', //loader do sass
+            ]
+        }]
+    }
+}
