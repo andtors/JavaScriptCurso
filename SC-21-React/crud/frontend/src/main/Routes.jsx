fdb5f8dbf5a7dc34ece6f591aa09573847f8aa7e
André Torres <andre.tors448@gmail.com>
2024-02-07 16:22:40 -0300

    SC-21-React projeto CRUD concluido

--- SC-21-React/crud/frontend/src/main/Routes.jsx
+++ SC-21-React/crud/frontend/src/main/Routes.jsx
@@ -4,7 +4,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "../components/home/Home";
 import UserCrud from "../components/user/UserCrud";
 
-export default props =>(
+export default props => (
 <Routes>
     <Route exact path="/" element={<Home />} />
     <Route path="/users" element={<UserCrud />} />
