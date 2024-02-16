dc02677b810216fcd7890eb7ddf2b86351f1a4ec
André Torres <andre.tors448@gmail.com>
2024-02-16 14:39:00 -0300

    SC-32-BancoRelacional Concusao

---
+++ SC-32-BancoRelacional/consultarComAgregacao.sql
@@ -0,0 +1,6 @@
+SELECT
+    regiao AS 'Região',
+    sum(populacao) AS Total
+FROM estados
+GROUP BY regiao    
+ORDER BY Total DESC
---
+++ SC-32-BancoRelacional/inserirCidades.sql
@@ -0,0 +1,16 @@
+SELECT * FROM estados
+SELECT * FROM cidades
+
+INSERT INTO cidades (nome, area, estado_id)
+VALUES ('Campinas', 795, 25)
+
+INSERT INTO cidades (nome, area, estado_id)
+VALUES ('Nitéroi', 133.9, 19)
+
+INSERT INTO cidades (nome, area, estado_id)
+VALUES ('Caruaru', 920.6, (SELECT id FROM estados WHERE sigla = 'PE'))
+
+INSERT INTO cidades (nome, area, estado_id)
+VALUES ('Juazeiro do Norte', 248.2, (SELECT id FROM estados WHERE sigla = 'CE'))
+
+SELECT * FROM cidades
