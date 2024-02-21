9d0ef17b294d6ba216365765fd519d49cbed516b
André Torres <andre.tors448@gmail.com>
2024-02-21 10:58:25 -0300

    SC-34-Express iniciado

---
+++ SC-34-Express/index.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const app = express()
+
+const saudacao = require('./saudacaoMid')
+
+app.use(saudacao('Guilherme'))
+
+app.use((req, res, next) => {
+    console.log('Antes...')
+    next()
+})
+
+
+app.get('/opa', (req, res, next) => {
+    console.log('Durante...')
+    res.json({
+        data: [
+            { id: 7, name: 'Ana', position: 1 },
+            { id: 34, name: 'Bia', position: 2 },
+            { id: 73, name: 'Carlos', position: 3 }
+        ],
+        count: 30,
+        skip: 0,
+        limit: 3,
+        status: 200
+    })
+
+    next()
+    /*  
+    res.json({
+        name: 'iPad 32gb',
+        price: 1899.00,
+        discount: 0.12
+    })
+    */
+
+    //res.send('<h1>Estou bem!</h1><br><br><h2>Tipo é HTML!</h2>')
+})
+
+app.use((req, res) => {
+    console.log('Depois...')
+})
+
+app.listen(3000, () => {
+    console.log('Backend executando...')
+})
+
