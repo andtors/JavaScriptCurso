fe9c8ab09bfc5f21b7a62bd0dde5a1b1e5a34e36
André Torres <andre.tors448@gmail.com>
2024-02-21 14:48:14 -0300

    SC-34-Express concluido

--- SC-34-Express/index.js
+++ SC-34-Express/index.js
@@ -1,15 +1,47 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 
 const saudacao = require('./saudacaoMid')
+const usuarioApi = require('./api/usuario')
+const produtoApi = require('./api/produto')
+produtoApi(app, 'com param!')
+
 
 app.use(saudacao('Guilherme'))
+app.post('/usuario', usuarioApi.salvar)
+app.get('/usuario', usuarioApi.obter)
+
+app.use(bodyParser.text())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
 
 app.use((req, res, next) => {
     console.log('Antes...')
     next()
 })
 
+app.get('/clientes/relatorio', (req, res) => {
+    res.send(`Cliente relatório: completo = ${req.query.completo} ano = ${req.query.ano}`)
+})
+
+app.post('/corpo', (req, res) => {
+    /*
+    let corpo = ''
+    req.on('data', function(parte) {
+        corpo += parte
+    })
+    req.on('end', function () {
+        res.send(corpo)
+    })
+    */
+    res.send(JSON.stringify(req.body))
+})
+
+app.get('/clientes/:id', (req, res) => {
+    res.send(`Cliente ${req.params.id} selecionado!`)
+})
 
 app.get('/opa', (req, res, next) => {
     console.log('Durante...')
